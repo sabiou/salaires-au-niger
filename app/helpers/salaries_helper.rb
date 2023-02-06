6f46cf84da9791d546a5e1996c420a304408ee65
Farouk Sabiou <sfarouk298@gmail.com>
2023-02-06 01:18:08 +0100

    add XOF currency formatter helper

--- app/helpers/salaries_helper.rb
+++ app/helpers/salaries_helper.rb
@@ -1,2 +1,14 @@
+include ActionView::Helpers::NumberHelper
+
 module SalariesHelper
+  def self.format_currency_to_xof(salary)
+    number_to_currency(
+      salary,
+      unit: "XOF",
+      separator: ".",
+      delimiter: ".",
+      format: "%n %u",
+      precision: 0
+    )
+  end
 end
