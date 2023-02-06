c109275868f4b93d4726c824d07dc88add52f46f
Farouk Sabiou <sfarouk298@gmail.com>
2023-02-06 01:26:42 +0100

    moved avg_salary to helper method

--- app/helpers/salaries_helper.rb
+++ app/helpers/salaries_helper.rb
@@ -1,6 +1,7 @@
 include ActionView::Helpers::NumberHelper
 
 module SalariesHelper
+  # XOF: West african currency used in Niger 🇳🇪
   def self.format_currency_to_xof(salary)
     number_to_currency(
       salary,
@@ -11,4 +12,9 @@ module SalariesHelper
       precision: 0
     )
   end
+
+  def self.avg_salary(arr)
+    format_currency_to_xof(arr.sum(&:salary) / arr.size)
+  end
+
 end
